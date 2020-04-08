b2a70ce105f71736d7c8d410e2bdc4e9b562348b
JCGuest <jcguest90@gmail.com>
2020-04-08 05:22:01 -0500

    logo and styles

--- frontend_js/src/index.js
+++ frontend_js/src/index.js
@@ -1,6 +1,6 @@
 const term = document.querySelector("#term")
 const loc = document.querySelector("#location")
-const btn = document.querySelector("body > form > button")
+const btn = document.querySelector("button")
 const players = document.querySelector("#players")
 
 btn.addEventListener('click', (e) => {
@@ -10,22 +10,37 @@ btn.addEventListener('click', (e) => {
     let numPlayers = players.value
      
     // fetch(`http://localhost:3000/games/search?term=${searchTerm}&location=${searchLocation}`)
-    const config = {
+    const gameConfig = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'}
     }
-    fetch('http://localhost:3000/games/create', config)
+
+    const userConfig = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'}
+    }
+//create game
+    fetch('http://localhost:3000/games/create', gameConfig)
+    .then(response => {
+        return response.json()
+    })
+    .then(json => {
+        let gameId  = json['data']['id']
+    })
+//create user
+    .then(gameId => {
+        fetch(`http://localhost:3000/users/create?game_id=${gameId}`, userConfig)
+    })
     .then(response => {
         return response.json()
     })
     .then(json => {
         console.log(json)
     })
-    
 
-    
-    })
+
 
     class Game {
         constructor(players, yelps) {
