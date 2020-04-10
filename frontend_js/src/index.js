7629b934a981c34f9498b99c03c162189e9f32dc
JCGuest <jcguest90@gmail.com>
2020-04-10 03:10:50 -0500

    creating class for yelps and pushing each instance to array that i will use for yelpRender()

--- frontend_js/src/index.js
+++ frontend_js/src/index.js
@@ -5,21 +5,21 @@ let players = document.querySelector("input#players")
 const userInfo = document.querySelector('form#user-info')
 userInfo.style['display'] = 'none'
 const yelps = document.querySelector('div#yelp-wrap')
-yelps.style['display'] = 'none'
+// yelps.style['display'] = 'none'
 
 function toggleUserName() {
     if (userInfo.style['display'] == 'none') {
         userInfo.style['display'] = 'block'
     } else {
         userInfo.style['display'] = 'none'
-    }
+        }
 }
 function toggleYelps() {
     if (yelps.style['display'] == 'none') {
         yelps.style['display'] = 'block'
     } else {
         yelps.style['display'] = 'none'
-    }
+        }
 }
 
 
@@ -28,41 +28,26 @@ btnNext.addEventListener('click', (e) => {
     TERM = term.value
     LOC = loc.value
     const playersInput = players.value
-     
-    // fetch(`http://localhost:3000/games/search?term=${TERM}&location=${LOC}`)
-    // .then(response => {
-    //     return response.json()
-    // })
-    // .then(json => {
-    //     return json['data']
-    //     // console.log(json)
-    // })
-    // .then(data => {
-    //  console.log("yelps data => ", data)
-    // })
-    // .catch(err => {
-    //     console.log(err)
-    // })
-//////////////////////////////////////////////////////
 
 //create game
-const gameConfig = {
+    const gameConfig = {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'}
-    }
+        };
+
     fetch(`http://localhost:3000/games/create`, gameConfig)
     .then(response => {
         return response.json()
-    })
+        })
     .then(json => {
         const gameId  = json['data']['id']
         return gameId
-    })
+        })
     .then(gameId => {
         console.log(gameId)
         createGame(gameId, playersInput)
-    })
+        })
     
 
 })
@@ -72,13 +57,13 @@ function createGame(gameId, numPlayers) {
     let playersArr = []
     const userParent = document.querySelector('div#parent')
     for (let i=1; i<= numPlayers-1; i++) {
-    let userInfo = document.querySelector('form#user-info')
-    let copy = userInfo.cloneNode(true)
-    userParent.appendChild(copy)
-    copy.querySelector('input').placeholder = ` player ${i+1}` 
-    copy.querySelector('input').id = `player-${i+1}-name`
-    playersArr.push(i)
-    }
+        let userInfo = document.querySelector('form#user-info')
+        let copy = userInfo.cloneNode(true)
+        userParent.appendChild(copy)
+        copy.querySelector('input').placeholder = ` player ${i+1}` 
+        copy.querySelector('input').id = `player-${i+1}-name`
+        playersArr.push(i)
+        }
     let playBtn = document.createElement('button')
     playBtn.innerHTML = "Play"
     userParent.appendChild(playBtn)
@@ -87,43 +72,91 @@ function createGame(gameId, numPlayers) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'}
-        }
+        };
     for (let i=1; i <= playersArr.length + 1; i++){
         let name = document.querySelector(`input#player-${i}-name`).value
         fetch(`http://localhost:3000/users/create?game_id=${gameId}&name=${name}`, userConfig)
         .then(response => {
             return response.json()
-        })
+            })
         .then(json => {
             console.log("user json =>", json)
-        })
+         })
         .catch(err => {
             console.log(err)
-        })
-    }
+            })
+        };
+    toggleYelps();
+    yelpFetch();
+    })
+
+}
+
+function yelpFetch() {
     const yelpConfig = {
-        method: 'POST',
+        method: "POST",
         headers: {
             'Content-Type': 'application/json'}
-        }
-        toggleYelps()
-        fetch(`http://localhost:3000/games/search?term=${TERM}&location=${LOC}`, yelpConfig)
+    };
+    fetch(`http://localhost:3000/games/search?term=${TERM}&location=${LOC}`, yelpConfig)
     .then(response => {
         return response.json()
-    })
+        })
     .then(json => {
-        console.log("yelp json =>", json)
-
-        return json['data']
-    })
+        // console.log(json)
+        return json['businesses']
+        })
     .then(data => {
-     console.log("yelps data=>",data)
-    })
+     console.log(data)
+        })
     .catch(err => {
         console.log(err)
     })
+};
+//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+let yelpsall = []
+function yelpClass(yelp, id) {
+    result = new Yelp(id, yelp['name'],yelp['url'],yelp['image-url'],yelp['price'],yelp['location']['address1'],yelp['phone'],yelp['rating'])
+    yelpsall.push(result)
+}
 
+class Yelp {
+    constructor(id, name, url, img, price, address, phone, rating ){
+        this.id = id
+        this.name = name
+        this.url = url 
+        this.img = img 
+        this.price = price 
+        this.address = address
+        this.phone = phone
+        this.rating = rating
+    }
+}
 
+function yelpRender(yelp) {
+    console.log(yelp.id)
+}
+
+function fetchy() {
+    const yelpConfig = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'}
+    };
+    fetch(`http://localhost:3000/games/search?term=coffee&location=austin`, yelpConfig)
+    .then(response => {
+        return response.json()
+        })
+    .then(json => {
+        return json['businesses']
+        })
+    .then(data => {
+     data.forEach( (yelp, i) => {
+         yelpClass(yelp, i+1)
+            })
+        })
+    .catch(err => {
+        console.log(err)
     })
-
 }
