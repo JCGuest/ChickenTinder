212cbbdf2b17d73b6c02b8539e5003f23c99e01d
JCGuest <jcguest90@gmail.com>
2020-04-13 23:41:15 -0500

    changed likes asocc. to user, dropped matches table

--- backend_rails/app/serializers/match_serializer.rb
+++
@@ -1,5 +0,0 @@
-class MatchSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :name, :yelp_id, :game
-  belongs_to :game
-end
--- backend_rails/db/schema.rb
+++ backend_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_183054) do
+ActiveRecord::Schema.define(version: 2020_04_14_043828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,13 +24,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_183054) do
   create_table "likes", force: :cascade do |t|
     t.string "name"
     t.string "yelp_id"
-    t.integer "game_id"
-  end
-
-  create_table "matches", force: :cascade do |t|
-    t.string "name"
-    t.string "yelp_id"
-    t.integer "game_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
